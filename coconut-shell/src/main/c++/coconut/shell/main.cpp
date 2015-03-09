a4f958c28289e6e013082fed055d12ba275f0b38
Mikołaj Radwan <mikoszrrr@gmail.com>
2015-03-09 13:16:44 +0100

    Copy resources target

--- coconut-shell/src/main/c++/coconut/shell/main.cpp
+++ coconut-shell/src/main/c++/coconut/shell/main.cpp
@@ -1,4 +1,6 @@
 #include <memory>
+#include <exception>
+#include <iostream>
 
 #include "coconut/milk/system/App.hpp"
 #include "game/Game.hpp"
@@ -12,10 +14,14 @@ using namespace coconut;
 using namespace coconut::shell;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow) {
-	std::shared_ptr<milk::system::App> app(new milk::system::App(hInstance, pScmdline, iCmdshow));
+	try {
+		std::shared_ptr<milk::system::App> app(new milk::system::App(hInstance, pScmdline, iCmdshow));
 
-	game::Game theGame(app);
-	theGame.loop();
+		game::Game theGame(app);
+		theGame.loop();
+	} catch (const std::exception& e) {
+		std::cerr << "Caught exception: " << e.what() << '\n';
+	}
 
 	return 0;
 }
